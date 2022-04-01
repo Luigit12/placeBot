b82f3ca61905b358024fc999a7f8924cec0bc482
luigit12 <58264257+Luigit12@users.noreply.github.com>
2022-04-01 19:09:40 +0200

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-import fetch from 'node-fetch';
-
-const response = await fetch('https://httpbin.org/post', {method: 'POST', body: 'a=1'});
-const data = await response.json();
-
-console.log(data);

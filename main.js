56fcf882f7a2cc7459a07866eedd1bf885452a0d
luigit12 <58264257+Luigit12@users.noreply.github.com>
2022-04-02 21:22:41 +0200

    dont skip pixels that failed (think about bearers)

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let beginx = 924
-let beginy = 971
+let beginx = 76
+let beginy = 78
 let x = beginx
 let y = beginy
 let color = 8
@@ -24,18 +24,8 @@ for (let i = 0; i < 10000; i++) {
         const lines = image.split('\n')
         x = chars + beginx
         y = rows + beginy
+        console.log(`(${x},${y})`)
 
-        if (chars > lines[rows].length) {
-            rows += 1
-            chars = 0
-        } else{
-            chars += 1
-        }
-        
-        if (rows > lines.length){
-            rows = 0
-            chars = 0
-        }
 
         if (lines[rows][chars] == "r") { // red
             color = 2
@@ -65,23 +55,48 @@ for (let i = 0; i < 10000; i++) {
             color = 25 
         } else if (lines[rows][chars] == "6") { // light gray
             color = 30
+        } else if (lines[rows][chars] == "q") { // black
+            color = 27
         } else { // if it doesnt recognize the character it just puts black
             color = 27
         }
 
+/*
+  "body": "{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":61,\"y\":466},\"colorIndex\":27,\"canvasIndex\":1}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}",
+  "method": "POST"
+*/
         const postResponse = await fetch("https://gql-realtime-2.reddit.com/query", {
         "headers": {
             "accept": "*/*",
             "authorization": account.trim(),
             "content-type": "application/json",
         },
-        "body": `{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":${x},\"y\":${y}},\"colorIndex\":${color},\"canvasIndex\":0}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}`,
+/*
+        "body": `{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":${x},\"y\":${y}},\"colorIndex\":${color},\"canvasIndex\":1}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}`,
         "method": "POST"
+*/
+        "body": `{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":${x},\"y\":${y}},\"colorIndex\":${color},\"canvasIndex\":1}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}`,
+        "method": "POST"
+  
         })
         const postData = await postResponse.json();
         console.log(postData)
         let date_ob = new Date();
         console.log(" at " + date_ob.getHours() + ":" + date_ob.getMinutes() + " account: " + account)
+
+        if (chars > lines[rows].length) {
+            rows += 1
+            chars = 0
+            console.log("new line")
+        } else if (postData.toString().indexOf("errors: [") != -1){
+            chars += 1
+        }
+        console.log(postData.toString().indexOf("errors: ["))
+        
+        if (rows > lines.length){
+            rows = 0
+            chars = 0
+        }
     }
     console.log(image)
     await sleep(302000) //wait 5 mins and 2 seconds for safety

02d9e106c015e500a9a9af9f251ce8ea596afbb4
luigit12 <58264257+Luigit12@users.noreply.github.com>
2022-04-03 12:52:45 +0200

    better for loop (now actually resets)

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
+let image = fs.readFileSync('92_2.txt', 'utf8')
+
 let beginx = 917
 let beginy = 486
 let x = beginx
@@ -15,88 +17,87 @@ function sleep(ms) {
     });
 }
 
-for (let i = 0; i < 10000; i++) {
-    let image = fs.readFileSync('92_2.txt', 'utf8')
-    let bearer = fs.readFileSync('bearer.txt', 'utf8')
-    let accounts = bearer.split('\n')
+for (let u = 0; u < 10; i++) {
+    for (let i = 0; i < image.length; i++) {
+        let image = fs.readFileSync('92_2.txt', 'utf8')
+        let bearer = fs.readFileSync('bearer.txt', 'utf8')
+        let accounts = bearer.split('\n')
 
-    for (const account of accounts) {
-        const lines = image.split('\n')
-        x = chars + beginx
-        y = rows + beginy
-        console.log(`(${x},${y})`)
+        for (const account of accounts) {
+            const lines = image.split('\n')
+            x = chars + beginx
+            y = rows + beginy
+            console.log(`(${x},${y})`)
 
-        if (chars > lines[rows].length) {
-            rows += 1
-            chars = 0
-            console.log("new line!")
+            if (chars > lines[rows].length) {
+                rows += 1
+                chars = 0
+                console.log("new line!")
+            }
+
+            if (lines[rows][chars] == "r") { // red
+                color = 2
+            } else if (lines[rows][chars] == "g") { // light green
+                color = 8
+            } else if (lines[rows][chars] == "b") { // dark blue
+                color = 12
+            } else if (lines[rows][chars] == "o") { // orange
+                color = 3
+            } else if (lines[rows][chars] == "x") { // gray
+                color = 29
+            } else if (lines[rows][chars] == "w") { // white
+                color = 31
+            } else if (lines[rows][chars] == "y") { // yellow
+                color = 4
+            } else if (lines[rows][chars] == "1") { // dark green
+                color = 6
+            } else if (lines[rows][chars] == "2") { // light blue
+                color = 14
+            } else if (lines[rows][chars] == "3") { // dark purple
+                color = 18
+            } else if (lines[rows][chars] == "p") { // purple
+                color = 19
+            } else if (lines[rows][chars] == "4") { // light pink
+                color = 23
+            } else if (lines[rows][chars] == "5") { // brown
+                color = 25 
+            } else if (lines[rows][chars] == "6") { // light gray
+                color = 30
+            } else if (lines[rows][chars] == "q") { // black
+                color = 27
+            } else { // if it doesnt recognize the character it just puts white
+                color = 31
+            }
+
+            const postResponse = await fetch("https://gql-realtime-2.reddit.com/query", {
+            "headers": {
+                "accept": "*/*",
+                "authorization": account.trim(),
+                "content-type": "application/json",
+            },
+            "body": `{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":${x},\"y\":${y}},\"colorIndex\":${color},\"canvasIndex\":0}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}`,
+            "method": "POST"
+
+            })
+            const postData = await postResponse.json();
+            const postString = JSON.stringify(postData)
+            //console.log(postData)
+            let date_ob = new Date();
+            console.log(" at " + date_ob.getHours() + ":" + date_ob.getMinutes() + " account: " + account)
+
+            if (postString.includes("error")){
+                console.log("error: ")
+                console.log(postString)
+
+            } else {
+                chars += 1
+            }
+            
+            console.log("\n")
         }
-
-        if (rows > lines.length){
-            rows = 0
-            chars = 0
-            console.log("reset")
-        }
-
-        if (lines[rows][chars] == "r") { // red
-            color = 2
-        } else if (lines[rows][chars] == "g") { // light green
-            color = 8
-        } else if (lines[rows][chars] == "b") { // dark blue
-            color = 12
-        } else if (lines[rows][chars] == "o") { // orange
-            color = 3
-        } else if (lines[rows][chars] == "x") { // gray
-            color = 29
-        } else if (lines[rows][chars] == "w") { // white
-            color = 31
-        } else if (lines[rows][chars] == "y") { // yellow
-            color = 4
-        } else if (lines[rows][chars] == "1") { // dark green
-            color = 6
-        } else if (lines[rows][chars] == "2") { // light blue
-            color = 14
-        } else if (lines[rows][chars] == "3") { // dark purple
-            color = 18
-        } else if (lines[rows][chars] == "p") { // purple
-            color = 19
-        } else if (lines[rows][chars] == "4") { // light pink
-            color = 23
-        } else if (lines[rows][chars] == "5") { // brown
-            color = 25 
-        } else if (lines[rows][chars] == "6") { // light gray
-            color = 30
-        } else if (lines[rows][chars] == "q") { // black
-            color = 27
-        } else { // if it doesnt recognize the character it just puts white
-            color = 31
-        }
-
-        const postResponse = await fetch("https://gql-realtime-2.reddit.com/query", {
-        "headers": {
-            "accept": "*/*",
-            "authorization": account.trim(),
-            "content-type": "application/json",
-        },
-        "body": `{\"operationName\":\"setPixel\",\"variables\":{\"input\":{\"actionName\":\"r/replace:set_pixel\",\"PixelMessageData\":{\"coordinate\":{\"x\":${x},\"y\":${y}},\"colorIndex\":${color},\"canvasIndex\":0}}},\"query\":\"mutation setPixel($input: ActInput!) {\\n  act(input: $input) {\\n    data {\\n      ... on BasicMessage {\\n        id\\n        data {\\n          ... on GetUserCooldownResponseMessageData {\\n            nextAvailablePixelTimestamp\\n            __typename\\n          }\\n          ... on SetPixelResponseMessageData {\\n            timestamp\\n            __typename\\n          }\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}`,
-        "method": "POST"
-
-        })
-        const postData = await postResponse.json();
-        const postString = JSON.stringify(postData)
-        //console.log(postData)
-        let date_ob = new Date();
-        console.log(" at " + date_ob.getHours() + ":" + date_ob.getMinutes() + " account: " + account)
-
-        if (postString.includes("error")){
-            console.log("error: ")
-            console.log(postString)
-
-        } else {
-            chars += 1
-        }
-        
-        console.log("\n")
+        await sleep(302000) //wait 5 mins and 2 seconds for safety
     }
-    await sleep(302000) //wait 5 mins and 2 seconds for safety
+    rows = 0
+    chars = 0
+    console.log("reset")
 }
